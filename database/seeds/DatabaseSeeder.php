c2f8a6b81e72744f5827e010e3038d80667ad52f
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-02-03 21:49:30 +0600

    Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,6 @@ class DatabaseSeeder extends Seeder
     {
          $this->call(UserSeeder::class);
          $this->call(CommentSeeder::class);
-         
+         $this->call(RoomSeeder::class);
     }
 }
