43736e3f3f102bde69b48caded593eddf32f4aa8
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2024-02-03 15:36:50 +0600

    distinct

--- database/factories/CommentFactory.php
+++ database/factories/CommentFactory.php
@@ -8,6 +8,6 @@ use Faker\Generator as Faker;
 $factory->define(Comment::class, function (Faker $faker) {
     return [
         'content' => $faker->text(500),
-        'user_id' => $faker->numberBetween(1,4)
+        'user_id' => $faker->numberBetween(1,3)
     ];
 });
